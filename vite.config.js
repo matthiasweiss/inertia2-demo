6d887d3fb2ff60deb66a7aa528a429f78ce351b2
Matthias Weiß <mail@matthiasweiss.at>
2025-02-10 18:41:34 +0100

    adapt workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { exec } from 'child_process';
+import fs from 'fs';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +11,29 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        {
+            name: 'generate-typescript-definitions',
+            configureServer() {
+                const path = 'app/Dto';
+
+                fs.watch(path, { recursive: true }, (event, filename) => {
+                    console.log(
+                        `File changed: ${filename}, running artisan command...`,
+                    );
+                    exec(
+                        'php artisan typescript:transform --format',
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                console.error(
+                                    `Error running artisan command: ${stderr}`,
+                                );
+                            } else {
+                                console.log(stdout);
+                            }
+                        },
+                    );
+                });
+            },
+        },
     ],
 });
